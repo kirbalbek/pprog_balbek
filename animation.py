7ae8a7e8df482418b5cc7648d172ddfa6b3a26d6
Kirill-inspiron <kbalbek@gmail.com>
2018-12-14 02:56:06 +0300

    v2 анимация, исправлены те же ошибки, что и в рисунке

--- animation.py
+++ animation.py
@@ -2,120 +2,109 @@ import graphics as gr
 import time
 
 window = gr.GraphWin("Otkritiye Arena", 600, 600)
-
-lawn = gr.Polygon(gr.Point(0,400),gr.Point(0,600),
-                  gr.Point(600,600), gr.Point(600,400))
+lawn = gr.Polygon(gr.Point(0, 400), gr.Point(0, 600),
+                  gr.Point(600, 600), gr.Point(600, 400))  # лужайка
 lawn.setFill('green')
-
-sky = gr.Polygon(gr.Point(0,0),gr.Point(0,400),
-                 gr.Point(600,400),gr.Point(600,0))
+sky = gr.Polygon(gr.Point(0, 0), gr.Point(0, 400),
+                 gr.Point(600, 400), gr.Point(600, 0))  # небо
 sky.setFill('blue')
 
 
-def draw_spartak(x):            #Лого Спартака и табличку под ним
-    romb = gr.Polygon(gr.Point(x-75,150),gr.Point(x,100),
-                      gr.Point(x+75,150), gr.Point(x,200))
+def draw_spartak(x):  # Рисует лого Спартака и табличку под ним
+    romb = gr.Polygon(gr.Point(x - 75, 150), gr.Point(x, 100),
+                      gr.Point(x + 75, 150), gr.Point(x, 200))
     romb.setFill('red')
     romb.draw(window)
-    stripe = gr.Polygon(gr.Point(x-55,164),gr.Point(x+20,113),
-                        gr.Point(x+55,137), gr.Point(x-20,188))
+    stripe = gr.Polygon(gr.Point(x - 55, 164), gr.Point(x + 20, 113),
+                        gr.Point(x + 55, 137), gr.Point(x - 20, 188))
     stripe.setFill('white')
     stripe.draw(window)
-    letterc = gr.Text(gr.Point(x, 150),"с")
+    letterc = gr.Text(gr.Point(x, 150), "с")
     letterc.setSize(35)
     letterc.setFill('red')
     letterc.draw(window)
-    plate = gr.Polygon(gr.Point(x-70,200),gr.Point(x-70,250),
-                       gr.Point(x+70,250), gr.Point(x+70,200))
+    plate = gr.Polygon(gr.Point(x - 70, 200), gr.Point(x - 70, 250),
+                       gr.Point(x + 70, 250), gr.Point(x + 70, 200))
     plate.setFill('white')
     plate.draw(window)
-    post = gr.Polygon(gr.Point(x-5,250),gr.Point(x-5,500),
-                      gr.Point(x+5,500), gr.Point(x+5,250))
+    post = gr.Polygon(gr.Point(x - 5, 250), gr.Point(x - 5, 500),
+                      gr.Point(x + 5, 500), gr.Point(x + 5, 250))
     post.setFill('white')
     post.draw(window)
-    lettername = gr.Text(gr.Point(x, 230),"Спартак Москва")
+    lettername = gr.Text(gr.Point(x, 230), "Спартак Москва")
     lettername.setSize(10)
     lettername.setFill('red')
     lettername.draw(window)
 
 
-def gate_bar(x):                #Рисует штанги
-    bar = gr.Line(gr.Point(x, 400),gr.Point(x, 300))
+def gate_bar(x):  # Рисует штанги
+    bar = gr.Line(gr.Point(x, 400), gr.Point(x, 300))
     bar.setWidth(10)
     bar.setOutline('gray')
     bar.draw(window)
 
 
-def net_right(x):               #Рисует сетку ворот справа
-    netr = gr.Line(gr.Point(x, 300),gr.Point(x+100, 400))
+def net_right(x):  # Рисует сетку ворот справа
+    netr = gr.Line(gr.Point(x, 300), gr.Point(x + 100, 400))
     netr.setWidth(3)
     netr.setOutline('white')
     netr.draw(window)
 
 
-def net_left(x):                 #Рисует сетку ворот слева
-    netr = gr.Line(gr.Point(x, 400),gr.Point(x+100, 300))
+def net_left(x):  # Рисует сетку ворот слева
+    netr = gr.Line(gr.Point(x, 400), gr.Point(x + 100, 300))
     netr.setWidth(3)
     netr.setOutline('white')
     netr.draw(window)
 
 
-lineright = gr.Line(gr.Point(450, 400),gr.Point(520, 600)) #Рисует разметку поля
-lineright.setWidth(15)
-lineright.setOutline('white')
+def liner_right(x):  # Рисует разметку справа
+    lin = gr.Line(gr.Point(x, 400), gr.Point(x + 70, 600))
+    lin.setWidth(15)
+    lin.setOutline('white')
+    lin.draw(window)
 
-linerleft = gr.Line(gr.Point(150, 400),gr.Point(80, 600))  #Рисует разметку поля
-linerleft.setWidth(15)
-linerleft.setOutline('white')
 
-gatebar_up = gr.Line(gr.Point(195, 300),gr.Point(405, 300)) #Верхняя штанга
+def liner_left(x):  # Рисует разметку слева
+    lin = gr.Line(gr.Point(x, 400), gr.Point(x - 70, 600))
+    lin.setWidth(15)
+    lin.setOutline('white')
+    lin.draw(window)
+
+
+gatebar_up = gr.Line(gr.Point(195, 300), gr.Point(405, 300))
 gatebar_up.setWidth(10)
-gatebar_up.setOutline('gray')
+gatebar_up.setOutline('gray')  # рисует перекладину ворот, в функцию вставлять не имеет смысла
 
-linerup = gr.Line(gr.Point(145, 400),gr.Point(455, 400))   #Рисует разметку поля
+linerup = gr.Line(gr.Point(145, 400), gr.Point(455, 400))
 linerup.setWidth(5)
 linerup.setOutline('white')
 
-ball = gr.Circle(gr.Point(350, 500), 20)                    #Рисует мяч
+ball = gr.Circle(gr.Point(350, 500), 20)  # рисует мяч
 ball.setFill('orange')
-
-bump = gr.Polygon(gr.Point(180,320),gr.Point(220,320),
-                  gr.Point(230,360), gr.Point(200,375),
-                   gr.Point(170,360))         #Рисует мишень для мяча на штанге
-bump.setFill('red')
-
 
 lawn.draw(window)
 sky.draw(window)
 draw_spartak(525)
 draw_spartak(75)
-lineright.draw(window)
-linerleft.draw(window)
+liner_right(450)
+liner_left(150)
 linerup.draw(window)
-net_right(200)
-net_right(225)
-net_right(250)
-net_right(275)
-net_right(300)
-net_left(200)
-net_left(225)
-net_left(250)
-net_left(275)
-net_left(300)
-gate_bar(200)
-gate_bar(400)
+for i in range(200, 301, 25):  # используется цикл, чтобы создать параллельные линии net_right и net_left
+    net_right(i)
+    net_left(i)
+for i in range(200, 401, 200):  # используется цикл, чтобы нарисовать параллельные штанги
+    gate_bar(i)
 gatebar_up.draw(window)
-bump.draw(window)
 ball.draw(window)
 
-while True==True:                  #Бесконечный цикл, в котором мяч....
-    for i in range (100):          #...перемещается с поля в мишень...
+if True is True:                  # Бесконечный цикл, в котором мяч....
+    for i in range(100):          # ...перемещается с поля в мишень...
         time.sleep(0.01)
-        ball.move(-1.5,-1.5)
-    for i in range (100):          #...и обратно
+        ball.move(-1.5, -1.5)
+    for i in range(100):          # ...и обратно
         time.sleep(0.01)
-        ball.move(1.5,1.5)
+        ball.move(1.5, 1.5)
 
 window.getMouse()
-
 window.close()
